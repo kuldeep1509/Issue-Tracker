7220a3836f7a958a2fd697cb5c1c123851ff8641
Kuldeep <kjadeja071@rku.ac.in>
2025-07-30 11:09:33 +0530

    changes made in settings.py

--- issue_tracker_backend/issue_tracker_backend/settings.py
+++ issue_tracker_backend/issue_tracker_backend/settings.py
@@ -278,10 +278,11 @@ INSTALLED_APPS = [
 
 # Middleware
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
