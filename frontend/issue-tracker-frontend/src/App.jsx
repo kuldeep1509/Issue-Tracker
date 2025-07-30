ed8c8fd030fbcf92b2d18846e1e95e4c6c568f01
Kuldeep <kjadeja071@rku.ac.in>
2025-07-30 11:39:32 +0530

    Change in App.jsx

--- frontend/issue-tracker-frontend/src/App.jsx
+++ frontend/issue-tracker-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Layout from './components/Layout';
